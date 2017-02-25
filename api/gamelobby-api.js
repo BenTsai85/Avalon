76a7704c822fb26505ee38c74a1e84158f54fc1f
yoyo <za8244@gmail.com>
2017-02-26 04:21:28 +0800

    0421

--- api/gamelobby-api.js
+++ api/gamelobby-api.js
@@ -2,11 +2,15 @@ import { Router } from 'express';
 import path from 'path';
 import assert from 'assert';
 import models from '../src/models';
+import bodyParser from 'body-parser';
+
 
 const { User } = models;
 const returnRouter = function ( io ) {
   const router = new Router();
-
+  router.use( bodyParser.urlencoded( { extended: false } ) );
+  router.use( bodyParser.json() );
+  
   io.on( 'connection', ( socket ) => {
     console.log("socket");
     socket.on( 'setHunmidity', Hunmidity => io.emit( 'setHunmidity', Hunmidity ) );
@@ -14,6 +18,22 @@ const returnRouter = function ( io ) {
     socket.on( 'RFID', RFID => io.emit( 'RFID', RFID ) );
    } );
 
+
+  router.post( '/setHunmidity', ( req, res ) => {
+    const { Hunmidity } = req.body;
+    io.emit( 'setHunmidity', Hunmidity );
+  } );
+
+
+  router.post( '/setTemperature', ( req, res ) => {
+    const { Temperature } = req.body;
+    io.emit( 'setTemperature', Temperature );
+  } );
+
+  router.post( '/RFID', ( req, res ) => {
+    const { RFID } = req.body;
+    io.emit( 'RFID', RFID );
+  } );
 
   router.post( '/', ( req, res ) => {
     gamerooms.push( { roomname: 'default room', player_num: 1, max_player_num: 10, id: gamerooms.length } );

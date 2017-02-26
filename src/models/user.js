3bab7698348512635ab88b0db162244effaa3d46
BenTsai85 <b03901028@ntu.edu.tw>
2017-02-26 09:17:31 +0800

    0917

--- src/models/user.js
+++ src/models/user.js
@@ -18,7 +18,7 @@ module.exports = ( sequelize, DataTypes ) => {
       allowNull: false,
       type: DataTypes.STRING,
     },
-    rest: {
+    surplus: {
       allowNull: false,
       type: DataTypes.INTEGER,
     },
